b8d24c63ca2d166382add893742c208f4e409409
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2022-12-04 20:58:47 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,10 +28,26 @@ const App = () => {
 
   }
   return (
-    <div className="landing">
-      <h1> Welcome dude! </h1>
+    <div className="mint-nft">
+      <h1>Mint your free 🍪DAO Membership NFT</h1>
+      <div className="btn-hero">
+        <Web3Button
+          contractAddress={editionDropAddress}
+          action={contract => {
+            contract.erc1155.claim(0, 1)
+          }}
+          onSuccess={() => {
+            console.log(`🌊 Successfully Minted! Check it out on OpenSea: https://testnets.opensea.io/assets/${editionDrop.getAddress()}/0`);
+          }}
+          onError={error => {
+            console.error("Failed to mint NFT", error);
+          }}
+        >
+          Mint your NFT (FREE)
+        </Web3Button>
+      </div>
     </div>
-  )
+  );
 
 };
 
