3edd5355001529d072d1a8833f77223cb58d9e50
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2022-12-04 18:10:29 +0700

    update deploy NFT script

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useAddress, ConnectWallet } from "@thirdweb-dev/react";
+
 const App = () => {
+  const address = useAddress();
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to My DAO</h1>
+        <div class="btn-hero">
+          <ConnectWallet />
+        </div>
+      </div>
+    );
+
+  }
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1> Welcome dude! </h1>
     </div>
-  );
+  )
+
 };
 
 export default App;
