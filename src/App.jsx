e0167c7606821f1bd04abc10c281861b9b87d173
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2022-12-04 18:23:19 +0700

    Set claim condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import { useAddress, ConnectWallet } from "@thirdweb-dev/react";
+import { useAddress, ConnectWallet, useNFTBalance, useContract } from "@thirdweb-dev/react";
+import { useState, useEffect, useMemo } from 'react';
 
 const App = () => {
   const address = useAddress();
+  console.log(address);
+  // Initialize our Edition Drop contract
+  const editionDropAddress = "0x0043C727F639972502D2c281d3dfB5bD874263d0"
+  const { contract: editionDrop } = useContract(editionDropAddress, "edition-drop");
+  // Hook to check if the user has our NFT
+  const { data: nftBalance } = useNFTBalance(editionDrop, address, "0")
+
+  const hasClaimedNFT = useMemo(() => {
+    return nftBalance && nftBalance.gt(0)
+  }, [nftBalance])
+
+  // ... include all your other code that was already there below.
 
   if (!address) {
     return (
